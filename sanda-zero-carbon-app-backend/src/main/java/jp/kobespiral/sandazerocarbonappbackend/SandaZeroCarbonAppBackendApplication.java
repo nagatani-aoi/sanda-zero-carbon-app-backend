3fc6259b2ce2e479eec2f3092dbb141cf521d0c7
nagatani-aoi <224x218x@stu.kobe-u.ac.jp>
2022-08-05 13:39:14 +0900

    Springプロジェクトを配備

---
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/SandaZeroCarbonAppBackendApplication.java
@@ -0,0 +1,13 @@
+package jp.kobespiral.sandazerocarbonappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandaZeroCarbonAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandaZeroCarbonAppBackendApplication.class, args);
+	}
+
+}
---
+++ sanda-zero-carbon-app-backend/src/test/java/jp/kobespiral/sandazerocarbonappbackend/SandaZeroCarbonAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.kobespiral.sandazerocarbonappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandaZeroCarbonAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

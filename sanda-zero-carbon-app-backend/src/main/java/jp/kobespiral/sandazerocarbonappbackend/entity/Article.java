b68f77b20be86eb5d7f452fba7e75590428cdfba
SatoShun333 <sato.shun@cs28.cs.kobe-u.ac.jp>
2022-08-05 14:35:35 +0900

    エンティティの作成

---
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/entity/Article.java
@@ -0,0 +1,35 @@
+package jp.kobespiral.sandazerocarbonappbackend.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ 記事エンティティ
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long articleId;
+    String title;
+    Long tagId;
+    String description;
+    @Temporal(TemporalType.TIMESTAMP)
+    Date postedAt;
+    String thumbnailSource;
+    Boolean isImportant;
+    String url;
+}
---
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/entity/UserDailyStatus.java
@@ -0,0 +1,36 @@
+package jp.kobespiral.sandazerocarbonappbackend.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/*
+ユーザデイリーステータスエンティティ
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class UserDailyStatus {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long userDailyStatusId;
+    Long userId;
+    @Temporal(TemporalType.TIMESTAMP)
+    Date date;
+    int totalPoint;
+    double totalCO2Reduction;
+    double totalCostReduction;
+    Boolean easyMissionCompleted;
+    Boolean nomalMissionCompleted;
+    Boolean hardMissionCompleted;
+}

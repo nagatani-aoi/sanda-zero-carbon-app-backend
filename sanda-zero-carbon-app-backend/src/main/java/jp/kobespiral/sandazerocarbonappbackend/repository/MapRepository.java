14465342be8c5a91e736361f1a65e52444812c90
Kamae4423 <kamae4423@gmail.com>
2022-08-05 17:08:50 +0900

    マップリポジトリの更新

--- sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/repository/MapRepository.java
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/repository/MapRepository.java
@@ -16,7 +16,7 @@ public interface MapRepository extends CrudRepository<Map, Long> {
      * マップ上での現在地とレベルを指定して，マップを返す
      * @param currentLocation マップ上での現在地
      * @param level レベル
-     * @return Optional<Map> マップ
+     * @return Map マップ
      */
     Map findByCurrentLocationAndLevel(Long currentLocation, int level);
 }

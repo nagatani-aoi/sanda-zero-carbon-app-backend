9c1909a0d3c4b4a36f47f0d9669057954b4c9078
nagatani-aoi <224x218x@stu.kobe-u.ac.jp>
2022-08-08 09:17:40 +0900

    ファイル階層整理 & エラー解消

---
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/domain/repository/AnsweredQuizRepository.java
@@ -0,0 +1,33 @@
+package jp.kobespiral.sandazerocarbonappbackend.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jp.kobespiral.sandazerocarbonappbackend.domain.entity.AnsweredQuiz;
+
+/**
+ * 回答済みクイズ リポジトリ
+ * 
+ * @author Kamae
+ */
+@Repository
+public interface AnsweredQuizRepository extends CrudRepository<AnsweredQuiz, Long> {
+    /**
+     * ユーザIDを指定して，回答済みクイズのリストを取得する
+     * 
+     * @param userId ユーザID
+     * @return List<AnsweredQuiz> 回答済みクイズのリスト
+     */
+    List<AnsweredQuiz> findByUserId(Long userId);
+
+    /**
+     * 正誤フラグとユーザIDを指定して，該当する回答済みリストを取得する
+     * 
+     * @param isCorrect 正誤フラグ
+     * @param userId    ユーザID
+     * @return List<AnsweredQuiz> 回答済みクイズのリスト
+     */
+    List<AnsweredQuiz> findByIsCorrectAndUserId(Boolean isCorrect, Long userId);
+}
---
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/domain/repository/MissionRepository.java
@@ -0,0 +1,33 @@
+package jp.kobespiral.sandazerocarbonappbackend.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import jp.kobespiral.sandazerocarbonappbackend.domain.entity.Mission;
+
+/**
+ * ミッションリポジトリ
+ * 
+ */
+@Repository
+public interface MissionRepository extends CrudRepository<Mission, Long> {
+
+    /**
+     * タイトルをキーワードで検索する．
+     * 
+     * @param keyword キーワード
+     * @return 指定されたキーワードをタイトルに含むミッションのリスト
+     */
+    List<Mission> findByDateTitleContaining(String keyword);
+
+    /**
+     * タグIDで記事を検索する
+     * 
+     * @param tagId タグID
+     * @return 指定されたタグIDでフィルター済みの記事のリスト
+     */
+    List<Mission> findByTagId(Long tagId);
+
+}

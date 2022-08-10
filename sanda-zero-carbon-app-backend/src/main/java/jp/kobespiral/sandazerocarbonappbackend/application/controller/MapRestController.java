9aeb6f586219e151160b407d66ec11d7c4f72474
Kamae4423 <kamae4423@gmail.com>
2022-08-10 14:46:13 +0900

    update

--- sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/application/controller/MapRestController.java
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/application/controller/MapRestController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jp.kobespiral.sandazerocarbonappbackend.application.dto.MapDto;
+import jp.kobespiral.sandazerocarbonappbackend.cofigration.exception.ErrorCode;
 import jp.kobespiral.sandazerocarbonappbackend.cofigration.exception.Response;
 import jp.kobespiral.sandazerocarbonappbackend.cofigration.exception.ResponseCreator;
+import jp.kobespiral.sandazerocarbonappbackend.cofigration.exception.UserValidationException;
 import jp.kobespiral.sandazerocarbonappbackend.domain.service.MapService;
+import static jp.kobespiral.sandazerocarbonappbackend.cofigration.exception.ErrorCode.*;
 import lombok.RequiredArgsConstructor;
 
 /**
@@ -34,10 +37,15 @@ public class MapRestController {
      * @return ラップされたマップDto
      */
     @GetMapping("/map")
-    @CrossOrigin("https://localhost:5173")
+    @CrossOrigin("http://localhost:5173")
     public Response<MapDto> getMap(@RequestParam("userId") String userId,
             @RequestParam("currentLocation") int currentLocation) {
-        return ResponseCreator.succeed(mapService.getMap(userId, currentLocation));
+        try{
+            return ResponseCreator.succeed(mapService.getMap(userId, currentLocation));
+        }
+        catch(Exception e){
+            return ResponseCreator.fail(ErrorCode.USER_DOES_NOT_EXIST,new UserValidationException(USER_DOES_NOT_EXIST,"get map", String.format("this user does not exist (userId: %d )",userId)),null);
+        }
     }
 
     /**
@@ -47,9 +55,14 @@ public class MapRestController {
      * @return ラップされたマップDto
      */
     @GetMapping("/map/initialLocation")
-    @CrossOrigin("https://localhost:5173")
+    @CrossOrigin("http://localhost:5173")
     public Response<MapDto> getMapOnInitialLocation(@RequestParam("userId") String userId) {
-        return ResponseCreator.succeed(mapService.getMapOnInitialLocation(userId));
+        try{
+            return ResponseCreator.succeed(mapService.getMapOnInitialLocation(userId));
+        }
+        catch(Exception e){
+            return ResponseCreator.fail(ErrorCode.USER_DOES_NOT_EXIST,new UserValidationException(USER_DOES_NOT_EXIST,"get map on initial location", String.format("this user does not exist (userId: %d )",userId)),null);
+        }
     }
 
 }

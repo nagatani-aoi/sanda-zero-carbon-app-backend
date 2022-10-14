a61f9b5d52d281a96205a2990ec46f2a592c3b3a
Kamae4423 <kamae4423@gmail.com>
2022-10-14 11:46:53 +0900

    クイズdtoを更新

--- sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/application/dto/AnsweredQuizDto.java
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/application/dto/AnsweredQuizDto.java
@@ -1,6 +1,9 @@
 package jp.kobespiral.sandazerocarbonappbackend.application.dto;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import jp.kobespiral.sandazerocarbonappbackend.domain.entity.AnsweredQuiz;
 import jp.kobespiral.sandazerocarbonappbackend.domain.entity.Quiz;
@@ -20,11 +23,13 @@ public class AnsweredQuizDto {
     String title; // タイトル
     String quizSentence; // 問題文
     String explaination; // 説明文
-    String ans1; // 選択肢１
-    String ans2; // 選択肢２
-    String ans3; // 選択肢３
-    String ans4; //選択肢４
-    String correctAns; //正解
+    List<String> answerList; //選択肢リスト
+    // String ans1; // 選択肢１
+    // String ans2; // 選択肢２
+    // String ans3; // 選択肢３
+    // String ans4; // 選択肢４
+
+    String correctAns; // 正解
     int point; // 獲得ポイント
     Long tagId; // タグID
     String keyword; // キーワード
@@ -42,10 +47,13 @@ public class AnsweredQuizDto {
         dto.title = quiz.getTitle();
         dto.quizSentence = quiz.getQuizSentence();
         dto.explaination = quiz.getExplaination();
-        dto.ans1 = quiz.getAns1();
-        dto.ans2 = quiz.getAns2();
-        dto.ans3 = quiz.getAns3();
-        dto.ans4 = quiz.getAns4();
+        List<String> list = new ArrayList<String>(Arrays.asList(quiz.getAns1(), quiz.getAns2(), quiz.getAns3(), quiz.getAns4()));
+        dto.answerList = list;
+        // dto.ans1 = quiz.getAns1();
+        // dto.ans2 = quiz.getAns2();
+        // dto.ans3 = quiz.getAns3();
+        // dto.ans4 = quiz.getAns4();
+
         dto.correctAns = quiz.getCorrectAns();
         dto.point = quiz.getPoint();
         dto.tagId = tag.getTagId();

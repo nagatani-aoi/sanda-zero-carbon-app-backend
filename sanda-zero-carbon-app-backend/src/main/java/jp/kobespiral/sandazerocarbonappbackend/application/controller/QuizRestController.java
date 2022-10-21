6f11fe6b1b5cdc6baa528119c6cd83fe83a27061
Kamae4423 <kamae4423@gmail.com>
2022-10-21 14:01:38 +0900

    Get→Post

--- sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/application/controller/QuizRestController.java
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/application/controller/QuizRestController.java
@@ -3,6 +3,7 @@ package jp.kobespiral.sandazerocarbonappbackend.application.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -37,7 +38,7 @@ public class QuizRestController {
      * @param form
      * @return 回答したクイズのDTO
      */
-    @GetMapping("/quiz/answer")
+    @PostMapping("/quiz/answer")
     Response<AnsweredQuizDto> answerQuiz(@Validated @RequestBody AnsweredQuizForm form) {
         return ResponseCreator.succeed(quizService.createAnsweredQuiz(form));
     }

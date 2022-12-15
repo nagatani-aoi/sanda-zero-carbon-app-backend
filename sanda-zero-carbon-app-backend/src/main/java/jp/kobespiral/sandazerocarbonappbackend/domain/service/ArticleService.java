520674fafd15b9263e9f0bc4dc28f527514a707c
nagatani-aoi <224x218x@stu.kobe-u.ac.jp>
2022-12-15 11:52:10 +0900

    記事サービスを作成

---
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/domain/service/ArticleService.java
@@ -0,0 +1,17 @@
+package jp.kobespiral.sandazerocarbonappbackend.domain.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 記事のサービス
+ * 
+ * @author sato
+ */
+
+@Service
+@RequiredArgsConstructor
+public class ArticleService {
+
+}

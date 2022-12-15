db2ba600dc07141c4ef9bdfd845250ee07fe0237
nagatani-aoi <224x218x@stu.kobe-u.ac.jp>
2022-12-15 12:14:27 +0900

    OGP作成途中

--- sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/application/dto/ArticleDto.java
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/application/dto/ArticleDto.java
@@ -1,6 +1,5 @@
 package jp.kobespiral.sandazerocarbonappbackend.application.dto;
 
-
 import java.time.LocalDateTime;
 
 import jp.kobespiral.sandazerocarbonappbackend.domain.entity.Article;
@@ -9,17 +8,30 @@ import lombok.Data;
 @Data
 public class ArticleDto {
 
-    Long articleId; //記事ID
-    String title; //タイトル
-    Long tagId; //タグID
-    String description; //説明
-    LocalDateTime postedAt; //投稿日時
-    String thumbnailSource; //サムネイルソース
-    Boolean isImportant; //重要かどうか
-    String url; //URL
+    Long articleId; // 記事ID
+    String title; // タイトル
+    Long tagId; // タグID
+    String description; // 説明
+    LocalDateTime postedAt; // 投稿日時
+    String thumbnailSource; // サムネイルソース
+    Boolean isImportant; // 重要かどうか
+    String url; // URL
 
-    
-    public static ArticleDto build(Article article){
+    public static ArticleDto build(Article article) {
+        ArticleDto dto = new ArticleDto();
+        dto.articleId = article.getArticleId();
+        dto.title = article.getTitle();
+        dto.tagId = article.getTagId();
+        dto.description = article.getDescription();
+        dto.postedAt = article.getPostedAt();
+        dto.thumbnailSource = article.getThumbnailSource();
+        dto.isImportant = article.getIsImportant();
+        dto.url = article.getUrl();
+        return dto;
+    }
+
+    public static ArticleDto buildOgp(Article article) {
+        // OGPの取得版に切り替える
         ArticleDto dto = new ArticleDto();
         dto.articleId = article.getArticleId();
         dto.title = article.getTitle();
--- sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/domain/service/ArticleService.java
+++ sanda-zero-carbon-app-backend/src/main/java/jp/kobespiral/sandazerocarbonappbackend/domain/service/ArticleService.java
@@ -1,17 +1,37 @@
 package jp.kobespiral.sandazerocarbonappbackend.domain.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import jp.kobespiral.sandazerocarbonappbackend.domain.entity.Article;
+import jp.kobespiral.sandazerocarbonappbackend.application.dto.ArticleDto;
+import jp.kobespiral.sandazerocarbonappbackend.domain.repository.ArticleRepository;
 import lombok.RequiredArgsConstructor;
 
 /**
  * 記事のサービス
  * 
- * @author sato
+ * @author ing
  */
 
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
+    private final ArticleRepository articleRepository;
 
+    /**
+     * 全ての記事を取得する
+     *
+     * @return 記事dtoリスト
+     */
+    public List<ArticleDto> getAllArticles() {
+        List<Article> articleList = articleRepository.findAllByOrderByPostedAtDesc();
+        List<ArticleDto> articleDtoList = new ArrayList<ArticleDto>();
+        for (Article list : articleList) {
+            articleDtoList.add(ArticleDto.build(list));
+        }
+        return articleDtoList;
+    }
 }
